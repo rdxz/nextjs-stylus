5b6b4da5d51166d3fd2bd75dc9359d02de0c7adf
stark <wsd312@163.com>
2019-11-04 15:20:21 +0800

    init

--- components/nav.js
+++ components/nav.js
@@ -2,8 +2,8 @@ import React from 'react'
 import Link from 'next/link'
 
 const links = [
-  { href: 'https://zeit.co/now', label: 'ZEIT' },
-  { href: 'https://github.com/zeit/next.js', label: 'GitHub' }
+  { href: '/blog', label: 'blog' },
+  { href: '/about', label: 'about' }
 ].map(link => {
   link.key = `nav-link-${link.href}-${link.label}`
   return link
